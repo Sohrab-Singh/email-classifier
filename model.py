f2c390a9d7ab0669648b4b589aef215eb21b8edd
Sohrab Singh Chawla <sohrabsinghchawla@gmail.com>
2019-04-04 00:21:55 -0400

    code for frequency count and conditional probabilities

--- model.py
+++ model.py
@@ -3,6 +3,9 @@ import re
 
 ham_email_dictionary = {}
 spam_email_dictionary = {}
+ham_word_probability = {}
+spam_word_probability = {}
+vocabulary = set()
 
 
 def parse_training_data():
@@ -14,6 +17,7 @@ def parse_training_data():
             words = re.split('[^a-zA-Z]',file_data_string)
             for word in words:
                 if word is not '':
+                    vocabulary.add(word)
                     if 'ham' in file:
                         if word in ham_email_dictionary:
                             ham_email_dictionary[word] = ham_email_dictionary[word] + 1
@@ -26,8 +30,17 @@ def parse_training_data():
                         else:
                             spam_email_dictionary[word] = 1
 
-    print(ham_email_dictionary)
-    print(spam_email_dictionary)
+    for word in vocabulary:
+        ham_email_word_count = ham_email_dictionary[word] if word in ham_email_dictionary else 0
+        spam_email_word_count = spam_email_dictionary[word] if word in spam_email_dictionary else 0
+        ham_word_probability[word] = ham_email_word_count/(ham_email_word_count + spam_email_word_count)
+        spam_word_probability[word] = spam_email_word_count/(ham_email_word_count + spam_email_word_count)
+
+    # print(ham_email_dictionary)
+    # print(spam_email_dictionary)
+    # print(ham_word_probability)
+    # print(spam_word_probability)
+    # print(vocabulary)
 
 
 
