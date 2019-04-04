ee8f2cf0eb90757d3e36a89e3a21ea127f90a1a5
sarthak3154 <sarthak.arora3154@gmail.com>
2019-04-04 04:18:06 -0400

    model word info formatting and file save

--- model.py
+++ model.py
@@ -35,21 +35,31 @@ def parse_training_data():
                         else:
                             spam_email_dictionary[word] = 1
                         spam_words_count += 1
+            f.close()
 
-    find_smoothed_conditional_probabilities()
+    build_training_model()
 
 
-def find_smoothed_conditional_probabilities(delta=0):
+def find_smoothed_conditional_probabilities(word, delta=0):
+    ham_email_word_count = ham_email_dictionary[word] if word in ham_email_dictionary else 0
+    spam_email_word_count = spam_email_dictionary[word] if word in spam_email_dictionary else 0
+    ham_word_probability[word] = (ham_email_word_count + delta)/(ham_words_count * (1 + delta))
+    spam_word_probability[word] = (spam_email_word_count + delta)/(spam_words_count * (1 + delta))
+
+
+def get_model_word_info(line_count, word):
+    return '{}  {}  {}  {}  {}  {}\n'.format(line_count, word, ham_email_dictionary.get(word, 0), ham_word_probability[word], spam_email_dictionary.get(word, 0), spam_word_probability[word])
+
+
+def build_training_model():
+    f = open(os.getcwd() + '/resources/model.txt', 'w+')
+    line_count = 0
     for word in vocabulary:
-        ham_email_word_count = ham_email_dictionary[word] if word in ham_email_dictionary else 0
-        spam_email_word_count = spam_email_dictionary[word] if word in spam_email_dictionary else 0
-        ham_word_probability[word] = (ham_email_word_count + delta)/(ham_words_count * (1 + delta))
-        spam_word_probability[word] = (spam_email_word_count + delta)/(spam_words_count * (1 + delta))
+        find_smoothed_conditional_probabilities(word)
+        line_count += 1
+        f.write(get_model_word_info(line_count, word))
 
-
-
-
-
+    f.close()
 
 
 
